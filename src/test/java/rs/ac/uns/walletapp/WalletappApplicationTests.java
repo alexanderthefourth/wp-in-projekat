d782f650f6c92372293af1c4ca21f4804acb48ac
alexanderthefourth <selimovic.in8.2023@uns.ac.rs>
2025-08-22 00:47:02 +0200

    added project boilerplate

---
+++ src/test/java/rs/ac/uns/walletapp/WalletappApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.uns.walletapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
