814cb5824c2a6c2f38248c9f12fd47be8b1ed0be
saska <aleksandrasedl@gmail.com>
2025-09-12 13:20:41 +0200

    izmene u repository

--- src/main/java/rs/ac/uns/walletapp/repository/TransactionRepository.java
+++ src/main/java/rs/ac/uns/walletapp/repository/TransactionRepository.java
@@ -3,5 +3,5 @@ package rs.ac.uns.walletapp.repository;
 import rs.ac.uns.walletapp.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TransactionRepository extends JpaRepository<Transaction, String> {
+public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
 }
