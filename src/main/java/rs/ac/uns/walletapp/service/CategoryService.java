3752a0ba2319ed36b1fbe324a862d88a9f3a2390
alexanderthefourth <selimovic.in8.2023@uns.ac.rs>
2025-09-16 21:36:23 +0200

    2.4 related changes

--- src/main/java/rs/ac/uns/walletapp/service/CategoryService.java
+++ src/main/java/rs/ac/uns/walletapp/service/CategoryService.java
@@ -2,6 +2,7 @@ package rs.ac.uns.walletapp.service;
 
 import org.springframework.stereotype.Service;
 import rs.ac.uns.walletapp.model.Category;
+import rs.ac.uns.walletapp.model.User;
 import rs.ac.uns.walletapp.repository.CategoryRepository;
 
 import java.util.List;
@@ -37,6 +38,27 @@ public class CategoryService {
 
     public void deleteCategory(int id) {
         categoryRepository.deleteById(id);
+    }
+
+    public List<Category> getCategoriesForUser(int userId) {
+        List<Category> categories = categoryRepository.findByPredefinedTrue();
+        categories.addAll(categoryRepository.findByUserId(userId));
+        return categories;
+    }
+
+    public Category createCategoryForUser(int userId, Category category) {
+        category.setPredefined(false);
+        User u = new User();
+        u.setId(userId);
+        category.setUser(u);
+        return categoryRepository.save(category);
+    }
+
+    public void deleteUserCategory(int userId, int categoryId) {
+        Category category = categoryRepository.findById(categoryId).orElse(null);
+        if (category != null && !category.isPredefined() && category.getUser().getId() == userId) {
+            categoryRepository.delete(category);
+        }
     }
 }
 
