edcfb40ff9e68a51dfe331b8c74779784c30a04e
alexanderthefourth <selimovic.in8.2023@uns.ac.rs>
2025-09-25 12:03:53 +0200

    crash fix

--- src/main/java/rs/ac/uns/walletapp/repository/GoalRepository.java
+++ src/main/java/rs/ac/uns/walletapp/repository/GoalRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface GoalRepository extends JpaRepository<Goal, Integer> {
-    Goal findByWalletId(int walletId);
-    List<Goal> findByUserId(int userId);
+    //Goal findByWalletId(int walletId);
+    //List<Goal> findByUserId(int userId);
 }
